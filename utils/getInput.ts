02b9b8ebce3f5c13f7e9270da3ff216ed7f78311
Laurens Schunk <laurensdschunk@gmail.com>
2025-10-18 12:10:28 -0600

    Test file integration

--- utils/getInput.ts
+++ utils/getInput.ts
@@ -1,8 +1,9 @@
 import { readFileSync } from "fs";
 import { join } from "path";
 
-export default function getInput(day: number): string {
+export default function getInput(day: number, test?: boolean): string {
   const dayStr = day.toString().padStart(2, "0");
-  const filePath = join(__dirname, `../../day${dayStr}/input.txt`);
+  const fileName = test === true? "test.txt" : "input.txt";
+  const filePath = join(__dirname, `../../day${dayStr}/${fileName}`);
   return readFileSync(filePath, "utf-8").trim();
 }
